c00e6320c4b30d2e708c75d3a5382f2c2df0ce8a
First Last <48098113+brookman1@users.noreply.github.com>
2021-08-31 14:57:59 +0000

    Added a function to print output.

--- main.py
+++ main.py
@@ -23,7 +23,13 @@ input_s = """[
 ]"""
 
 
-    
-input_list = json.loads(input_s)
-print(json.dumps(optimal_order_output2(input_list)).replace('}, ', '},\n'))
-#pprint.pprint(optimal_order_output2(input_list)) 
+def json_output_optimal_order(input_s):
+  """
+  input_s: proper JSONstring, the comman that was on line 7 won't pass.
+  """
+  input_list = json.loads(input_s)
+  return json.dumps(optimal_order_output2(input_list))
+  
+if __name__ == '__main__':
+  print(json_output_optimal_order(input_s).replace('}, ', '},\n'))
+  #pprint.pprint(optimal_order_output2(input_list)) 
